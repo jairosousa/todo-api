8c2d70a8958bc926eeb737923b4a3571e56751d6
jaironsousa1 <jaironsousa@gmail.com>
2020-11-20 18:18:30 -0300

    Criando a entidade e configurando o lombok

---
+++ api/todo/src/main/java/io/jairosousa/todo/model/Todo.java
@@ -0,0 +1,30 @@
+package io.jairosousa.todo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String descriptions;
+
+    @Column
+    private Boolean done;
+
+    @Column
+    private LocalDateTime createdate;
+
+    @Column
+    private LocalDateTime doneDate;
+
+}
