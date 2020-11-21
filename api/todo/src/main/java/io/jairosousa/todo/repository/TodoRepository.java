90b9080aa430712803a5382f065a3176380090ab
jaironsousa1 <jaironsousa@gmail.com>
2020-11-21 13:51:58 -0300

    alteração pacote

--- api/todo/src/main/java/io/jairosousa/todo/repository/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package io.jairosousa.todo.repository;
-
-import io.jairosousa.todo.model.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  url: 'https://jns-todo-api.herokuapp.com',
+  endpointTodo: 'api/todos'
+};
