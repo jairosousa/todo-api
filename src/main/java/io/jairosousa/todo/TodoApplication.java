9f85d63df31433ea018ee649ebf9b216966e45f4
jaironsousa1 <jaironsousa@gmail.com>
2020-11-21 19:29:06 -0300

    Troca Bean Locale

--- src/main/java/io/jairosousa/todo/TodoApplication.java
+++ src/main/java/io/jairosousa/todo/TodoApplication.java
@@ -2,12 +2,22 @@ package io.jairosousa.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class TodoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver(){
+		return new FixedLocaleResolver(new Locale("pt", "BR"));
 	}
 
 }
